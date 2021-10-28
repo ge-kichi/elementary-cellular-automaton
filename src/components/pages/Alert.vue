c78836df17af818bd7ae6314a8cbe2c2864bfddb
Rikuo Kono <terrestrial1116@gmail.com>
2021-10-29 01:50:56 +0900

    commit

--- src/components/pages/Alert.vue
+++ src/components/pages/Alert.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Alert">
     <p class="nes-text is-warning">
-      Sorry! Please rotate your smartphone from portrait to landscape!
+      Sorry! Please rotate your smartphone from landscape to portrait!
     </p>
   </div>
 </template>
